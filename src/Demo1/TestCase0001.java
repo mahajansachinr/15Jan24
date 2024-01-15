406d6e907a3330906b1fcb17c2b92487a2ee42ca
mahajansachinr <mahajansachinr@gmail.com>
2024-01-15 23:00:06 +0530

    [sachin] : new code added

---
+++ src/Demo1/TestCase0001.java
@@ -0,0 +1,27 @@
+package Demo1;
+
+public class TestCase0001 {
+
+	public static void main(String[] args) {
+		TestCase0001 mobj = new TestCase0001();
+		mobj.m1();
+		
+
+	}
+
+	public void m1()
+
+	{
+		System.out.println("m1 method calling ");
+
+			}
+
+	public void m2()
+
+	{
+		System.out.println("m2 method calling ");
+		
+		
+	}
+
+}
